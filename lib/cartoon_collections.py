7e951a488666114b3b21646a20effbef6e0779ed
Ann Ndiga <annndiga@gmail.com>
2023-08-23 20:51:49 +0300

    Done assignment

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,21 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves):
+   for index, dwarf in enumerate(dwarves, start=1):
+        print(f"{index}. {dwarf}")
 
-def summon_captain_planet():
-    pass
 
-def long_planeteer_calls():
-    pass
+def summon_captain_planet(captain_calls):
+    return [call.capitalize() + '!' for call in captain_calls]
 
-def find_the_cheese():
-    pass
+
+def long_planeteer_calls(calls):
+    for call in calls:
+        if len(call) > 4:
+            return True
+    return False
+
+def find_the_cheese(snacks):
+    cheeses = ["cheddar", "gouda", "camembert"]
+    for snack in snacks:
+        if snack in cheeses:
+            return snack
+    return None
